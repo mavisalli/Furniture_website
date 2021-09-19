21a9ace937b9d7626b575b50f16f07a2f4aa88b1
mavi barış <mavi.baris96@gmail.com>
2021-09-19 15:57:04 +0300

    update

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,15 +1,10 @@
-const express = require('express')
-const authController = require('../controllers/authController');
-
-
-
+const express = require("express");
+const authController = require("../controllers/authController");
 
 const router = express.Router();
 
-router.route('/signup').post(authController.createAdmin); // http://localhost:3000/users/signup
-router.route('/loginpage').post(authController.loginAdmin);
-router.route('/logout').get(authController.logoutAdmin);
-
-
+router.route("/signup").post(authController.createAdmin); // http://localhost:5000/users/signup
+router.route("/loginpage").post(authController.loginAdmin);
+router.route("/logout").get(authController.logoutAdmin);
 
 module.exports = router;
--- routes/shopRoute.js
+++ routes/shopRoute.js
@@ -3,21 +3,16 @@ const express = require("express");
 const shopController = require("../controllers/shopController");
 const redirectMiddleware = require("../middlewares/redirectMiddleware");
 
-
 const router = express.Router();
 
 router.route("/").get(shopController.getIndexPage);
 router.route("/about-us").get(shopController.getAboutPage);
 router.route("/store").get(shopController.getStorePage);
 router.route("/contact").get(shopController.getContactPage);
+router.route("/sss").get(shopController.getSssPage);
 router.route("/login").get(redirectMiddleware, shopController.getLoginPage);
 router.route("/products").get(shopController.getAllProducts);
 router.route("/products/:slug").get(shopController.getProduct);
 router.route("/contacts").post(shopController.sendMail);
-
-
-
-
-
 
 module.exports = router;
