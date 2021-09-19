07f5bad267bcb3ae9b98d7fb4d9c55fc8e0e7a10
mavi barış <mavi.baris96@gmail.com>
2021-09-19 15:48:46 +0300

    convert image in model to array

--- models/Product.js
+++ models/Product.js
@@ -1,58 +1,56 @@
 const mongoose = require("mongoose");
-const slugify = require('slugify');
+const slugify = require("slugify");
 const Schema = mongoose.Schema;
 
-
-const ProductSchema = new Schema ({
-  
-    name: {
-        type: String,
-        unique: true,
-        required: true,
+const ProductSchema = new Schema({
+  name: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  images: [
+    {
+      type: String,
+      required: true,
     },
-    image: {
-        type: String,
-        required: true,
-    },
-    size: {
-        type: String,
-        required: true
-
-    }, 
-    description: {
-        type: String,
-        required: true
-    },
-    moreInfo: {
-        type: String
-    },
-    color: {
-        type: String
-    },
-    category: {
-        type:mongoose.Schema.Types.ObjectId,
-        ref:'Category'
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now
-    },
-    slug: {
-        type: String,
-        unique: true
-    },
-      
+  ],
+  size: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  moreInfo: {
+    type: String,
+  },
+  color: {
+    type: String,
+  },
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Category",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  slug: {
+    type: String,
+    unique: true,
+  },
 });
 
-ProductSchema.pre("validate", function(next){  //arrow func yapmadık cünkü this özelligi yok onda.
-    this.slug = slugify(this.name, {
-        lower: true,  // stringi kücük harfe cevirir
-        strict: true  //özel karakterleri keser
-    })
-    next();
-})
+ProductSchema.pre("validate", function (next) {
+  //arrow func yapmadık cünkü this özelligi yok onda.
+  this.slug = slugify(this.name, {
+    lower: true, // stringi kücük harfe cevirir
+    strict: true, //özel karakterleri keser
+  });
+  next();
+});
 
-const Product = mongoose.model("Product", ProductSchema); 
+const Product = mongoose.model("Product", ProductSchema);
 
-
-module.exports= Product;
+module.exports = Product;
